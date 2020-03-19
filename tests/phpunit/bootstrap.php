74bd56478f32cec7d454b1802c54fe721525cc0b
Jake Jackson <jake@gravitypdf.com>
2020-03-19 16:10:10 +1100

    Add unit testing scaffold

---
+++ tests/phpunit/bootstrap.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Gravity_Pdf_Bulk_Generator
+ */
+if ( function_exists( 'xdebug_disable' ) ) {
+	xdebug_disable();
+}
+
+class GravityPDF_Bulk_Generator_Unit_Tests_Bootstrap {
+
+	/** @var string directory where wordpress-tests-lib is installed */
+	public $wp_tests_dir;
+
+	/** @var string testing directory */
+	public $tests_dir;
+
+	/** @var string plugin directory */
+	public $plugin_dir;
+
+	/** @var \Monolog\Logger */
+	public $log;
+
+	/**
+	 * GravityPDF_Bulk_Generator_Unit_Tests_Bootstrap constructor.
+	 */
+	public function __construct() {
+
+		$this->tests_dir    = dirname( __FILE__ );
+		$this->plugin_dir   = dirname( $this->tests_dir ) . '/..';
+		$this->wp_tests_dir = $this->plugin_dir . '/tmp/wordpress-tests-lib';
+
+		/* load test function so tests_add_filter() is available */
+		require_once $this->wp_tests_dir . '/includes/functions.php';
+
+		/* load Gravity PDF */
+		tests_add_filter( 'muplugins_loaded', [ $this, 'load' ] );
+
+		/* load the WP testing environment */
+		require_once( $this->wp_tests_dir . '/includes/bootstrap.php' );
+	}
+
+	/**
+	 * Load Gravity Forms and Gravity PDF
+	 */
+	public function load() {
+		require_once $this->plugin_dir . '/tmp/gravityforms/gravityforms.php';
+		require_once $this->plugin_dir . '/tmp/gravity-forms-pdf-extended/pdf.php';
+
+		/* set up Gravity Forms database */
+		RGFormsModel::drop_tables();
+		( function_exists( 'gf_upgrade' ) ) ? gf_upgrade()->maybe_upgrade() : @GFForms::setup( true );
+
+		require $this->plugin_dir . '/gravity-pdf-bulk-generator.php';
+
+		/* Setup testing logger */
+		require $this->plugin_dir . '/tmp/gravity-forms-pdf-extended/vendor/autoload.php';
+		$this->log = new \Monolog\Logger( 'test' );
+		$this->log->pushHandler( new \Monolog\Handler\NullHandler( \Monolog\Logger::INFO ) ); /* throw logs away */
+	}
+}
+
+$GLOBALS['GFPDF_Test'] = new GravityPDF_Bulk_Generator_Unit_Tests_Bootstrap();
